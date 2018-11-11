eee86bd915cf58c8d1d2bd3c115f335bac25e28c
Jeremy Cowgar <jeremy@cowgar.com>
2018-11-11 14:18:25 -0500

    Apply styling from balloon theme
    This is a first attempt

--- config.def.h
+++ config.def.h
@@ -1,21 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const unsigned int borderpx      = 2;        /* border pixel of windows */
+static const unsigned int snap          = 32;       /* snap pixel */
+static const int showbar                = 1;        /* 0 means no bar */
+static const int topbar                 = 1;        /* 0 means bottom bar */
+static const char *fonts[]              = { "monospace:size=12" };
+static const char dmenufont[]           = "monospace:size=12";
+static const char colorNormalFg[]       = "#a5c6d7";
+static const char colorNormalBg[]       = "#397887";
+static const char colorNormalBorder[]   = "#090a09";
+static const char colorSelectedFg[]     = "#ffffff";
+static const char colorSelectedBg[]     = "#916497";
+static const char colorSelectedBorder[] = "#e56b8b";
+static const char *colors[][3] = {
+	[SchemeNorm] = { colorNormalFg,   colorNormalBg,   colorNormalBorder },
+	[SchemeSel]  = { colorSelectedFg, colorSelectedBg, colorSelectedBorder },
 };
 
 /* tagging */
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", colorNormalBg, "-nf", colorNormalFg, "-sb", colorSelectedFg, "-sf", colorSelectedBg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
